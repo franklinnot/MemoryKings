361747661385474ea89ffa922742b28f83901db4
franklinnot <jhonfrank479@gmail.com>
2024-04-25 12:19:12 -0500

    equis

--- MemoryKings/src/persistencia/Conexion.java
+++ MemoryKings/src/persistencia/Conexion.java
@@ -1,9 +1,26 @@
 
 
 package persistencia;
-
+import java.sql.Date;
 
 public class Conexion {
+    
+    Date fecha_conexion;
+
+    public Conexion(Date fecha_conexion) {
+        this.fecha_conexion = fecha_conexion;
+    }
+    
+    public Conexion(){}
+
+    public Date getFecha_conexion() {
+        return fecha_conexion;
+    }
+
+    public void setFecha_conexion(Date fecha_conexion) {
+        this.fecha_conexion = fecha_conexion;
+    }
+    
     
     
 }
