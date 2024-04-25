8463b123038df795e10f1659fbe09bdcbe92c002
franklinnot <jhonfrank479@gmail.com>
2024-04-25 12:23:10 -0500

    segundo atributo

--- MemoryKings/src/persistencia/Conexion.java
+++ MemoryKings/src/persistencia/Conexion.java
@@ -6,9 +6,11 @@ import java.sql.Date;
 public class Conexion {
     
     Date fecha_conexion;
+    String tipo_conexion;
 
-    public Conexion(Date fecha_conexion) {
+    public Conexion(Date fecha_conexion, String tipo_conexion) {
         this.fecha_conexion = fecha_conexion;
+        this.tipo_conexion = tipo_conexion;
     }
     
     public Conexion(){}
